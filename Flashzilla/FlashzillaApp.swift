b0caab45929896807398b24eb4f57e9e10ac7e15
dnstrach <dnstrach@gmail.com>
2024-01-11 12:38:23 -0800

    initial commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Dominique Strachan on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
